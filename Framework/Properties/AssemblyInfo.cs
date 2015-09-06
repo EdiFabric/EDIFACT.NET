27d8ed12c90646f216220f93e76a1b84a2f8ea29
DonZoeggerle <azagal@gmail.com>
2015-09-06 21:25:31 +0100

    Updated assembly version

--- Framework/Properties/AssemblyInfo.cs
+++ Framework/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EdiFabric.Framework.Parsers.Interchange")]
+[assembly: AssemblyTitle("EdiFabric.Framework")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("EdiFabric.Framework.Parsers.Interchange")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyProduct("EdiFabric.Framework")]
+[assembly: AssemblyCopyright("Copyright © ediFabric 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("6.0.4.0")]
-[assembly: AssemblyFileVersion("6.0.4.0")]
+[assembly: AssemblyVersion("6.1.0.0")]
+[assembly: AssemblyFileVersion("6.1.0.0")]
