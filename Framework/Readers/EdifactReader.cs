f6580c9667b97d5fc4010f5e3d9c79d4097a434a
DonZoeggerle <azagal@gmail.com>
2016-10-03 09:34:31 +0100

    Removed CreateReader overload

--- Framework/Readers/EdifactReader.cs
+++ Framework/Readers/EdifactReader.cs
@@ -43,19 +43,6 @@ namespace EdiFabric.Framework.Readers
         public static EdifactReader Create(Stream ediStream, string rulesAssemblyName = null, Encoding encoding = null, string rulesNamespacePrefix = null)
         {
             return new EdifactReader(ediStream, rulesAssemblyName, encoding, rulesNamespacePrefix);
-        }
-
-        /// <summary>
-        /// Factory method to initialize a new instance of the <see cref="EdifactReader"/> class.
-        /// </summary>
-        /// <param name="ediStream">The EDI stream to read from.</param>
-        /// <param name="rulesAssemblyName">The full assembly name of the assembly containing the EDI classes. The default is Edifabric.Rules.</param>
-        /// <param name="encoding">The encoding. The default is Encoding.Default.</param>
-        /// <param name="rulesNamespacePrefix">The namespace prefix for the EDI classes. The default is Edifabric.Rules.</param>
-        /// <returns>A new instance of the <see cref="EdifactReader"/> class.</returns>
-        public static EdifactReader Create(Stream ediStream, Encoding encoding = null, string rulesAssemblyName = null, string rulesNamespacePrefix = null)
-        {
-            return new EdifactReader(ediStream, rulesAssemblyName, encoding, rulesNamespacePrefix);
-        }  
+        } 
     }
 }
--- Framework/Readers/X12Reader.cs
+++ Framework/Readers/X12Reader.cs
@@ -44,18 +44,5 @@ namespace EdiFabric.Framework.Readers
         {
             return new X12Reader(ediStream, rulesAssemblyName, encoding, rulesNamespacePrefix);
         }
-
-        /// <summary>
-        /// Factory method to initialize a new instance of the <see cref="X12Reader"/> class.
-        /// </summary>
-        /// <param name="ediStream">The EDI stream to read from.</param>
-        /// <param name="rulesAssemblyName">The full assembly name of the assembly containing the EDI classes. The default is Edifabric.Rules.</param>
-        /// <param name="encoding">The encoding. The default is Encoding.Default.</param>
-        /// <param name="rulesNamespacePrefix">The namespace prefix for the EDI classes. The default is Edifabric.Rules.</param>
-        /// <returns>A new instance of the <see cref="X12Reader"/> class.</returns>
-        public static X12Reader Create(Stream ediStream, Encoding encoding = null, string rulesAssemblyName = null, string rulesNamespacePrefix = null)
-        {
-            return new X12Reader(ediStream, rulesAssemblyName, encoding, rulesNamespacePrefix);
-        } 
     }
 }
