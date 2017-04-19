94f1e22cd1ecb93e17261341bd1f4dbd5334110c
DonZoeggerle <azagal@gmail.com>
2017-04-19 21:05:27 +0100

    Removed unused test files

--- Rules/EF_EDIFACT_D00A_Enums.cs
+++
@@ -1,9 +0,0 @@
-namespace EdiFabric.Rules.EDIFACT_D00A
-{
-    using System;
-    using System.Xml;
-    using System.Xml.Serialization;
-    using System.Collections.Generic;
-    using EdiFabric.Attributes;
-    
-}
--- Rules/EF_X12_002040_Enums.cs
+++
@@ -1,9 +0,0 @@
-namespace EdiFabric.Rules.X12_002040
-{
-    using System;
-    using System.Xml;
-    using System.Xml.Serialization;
-    using System.Collections.Generic;
-    using EdiFabric.Attributes;
-    
-}
