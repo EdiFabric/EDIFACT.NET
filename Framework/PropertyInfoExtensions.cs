9f66292c6ba7d7c70e3475b4d94d30bd39ef977d
DonZoeggerle <azagal@gmail.com>
2017-05-16 17:43:55 +0100

    Propertyinfo extensions refactoring

--- Framework/PropertyInfoExtensions.cs
+++ Framework/PropertyInfoExtensions.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Reflection;
 using EdiFabric.Core.Annotations.Edi;
 
-namespace EdiFabric.Core
+namespace EdiFabric.Framework
 {
     /// <summary>
     /// PropertyInfo extensions
